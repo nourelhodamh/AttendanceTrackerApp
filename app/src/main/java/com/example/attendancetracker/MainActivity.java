e92685c8cd992688cb71637cd77fa3d80215b5df
nour <nourelhoudamh13@gmail.com>
2019-04-08 18:29:57 +0200

    implementing dynamic Broadcast Receiver+ Connection to Firebase database

--- app/src/main/java/com/example/attendancetracker/MainActivity.java
+++ app/src/main/java/com/example/attendancetracker/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.attendancetracker;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.res.ColorStateList;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Resources;
+import android.drm.DrmStore;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Bundle;
 import android.support.constraint.Group;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,9 +37,22 @@ public class MainActivity extends AppCompatActivity {
     private TextView wifiStatus;
     private TextView checkedOut;
     private TextView checkedIn;
-    Group group1;
-    Group group2;
-    String myNetworkName = "KabelBox-9C1C";
+    private TextView leftAt;
+    private Group group1;
+    private Group group2;
+    private String myNetworkName = "KabelBox-9C1C";
+    private ConnectivityManager connManager;
+    private NetworkInfo activeNetwork;
+    String y;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    private int connectedColor;
+    private int disconnectedColor;
+
+
+    String TAG;
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,56 +60,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         wifiStatus = findViewById(R.id.text_wifi_status);
+        checkedIn = findViewById(R.id.txt_checked_in);
+        checkedOut = findViewById(R.id.txt_checked_out);
+        leftAt = findViewById(R.id.txt_left_at);
         date = findViewById(R.id.text_date);
+        btn = findViewById(R.id.btn);
+
+        /*Network*/
+        connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        activeNetwork = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
         /*Accessing App resources*/
         Resources res = getResources();
+        connectedColor = res.getColor(R.color.colorGreen);
+        disconnectedColor = res.getColor(R.color.colorRed);
 
-
-        /*Calling Date function*/
+        /*Get The date*/
         getDate();
 
-        /*Check connectivity*/
-        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-
         /*Checking connectivity to a specific wifi*/
-        if (mWifi.isConnected()) {
+        if (activeNetwork.isConnected()) {
             String x = returnWifiName();
-            String y = x.replaceAll("^\"|\"$", "");
-            //Toast.makeText(getApplicationContext(), "wifi connected ", Toast.LENGTH_LONG).show();
+            y = x.replaceAll("^\"|\"$", "");
+            Toast.makeText(getApplicationContext(), "wifi connected ", Toast.LENGTH_LONG).show();
             /*network name Toast */
-            //Toast.makeText(getApplicationContext(), "connected to" + x, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "connected to" + x, Toast.LENGTH_LONG).show();
             if (y.equals(myNetworkName)) {
-                wifiStatus.setText("You are connected");
-                int connectedColor = res.getColor(R.color.colorGreen);
-                wifiStatus.setTextColor(connectedColor);
-            } else if (mWifi.isConnected() && !(y.equals(myNetworkName))) {
-                wifiStatus.setText("You are  not connected");
-                int disconnectedColor = res.getColor(R.color.colorRed);
-                wifiStatus.setTextColor(disconnectedColor);
-                //Toast.makeText(getApplicationContext(), "You are not connected to your workplace WI-FI",
-                // Toast.LENGTH_LONG).show();
+                connectedToNetwork();
+            } else if (activeNetwork.isConnected() && !(y.equals(myNetworkName))) {
+                disconnectedToNetwork();
+
+                Toast.makeText(getApplicationContext(), "You are not connected to your workplace WI-FI",
+                        Toast.LENGTH_LONG).show();
             }
         } else {
 
-            Toast.makeText(getApplicationContext(), "Please turn ON your WI-FI", Toast.LENGTH_LONG).show();
+            //Toast.makeText(getApplicationContext(), "Please turn ON your WI-FI", Toast.LENGTH_LONG).show();
         }
 
 
+        /*Connecting to FirebaseDatabase*/
+        database = FirebaseDatabase.getInstance();
+
+        //get reference to Arriving Node
+        myRef = database.getReference("Arriving");
+        myRef.child("Status").setValue("Yes");
+
+        /*get reference to Departure Node*/
+        myRef = database.getReference("Departure");
+        myRef.child("Status").setValue("No");
+
+
+        /*Saving timeStamp of arrival to FirebaseDatabase*/
+        Map map = new HashMap();
+        map.put("Time Stamp", ServerValue.TIMESTAMP);
+        myRef.child("Time").updateChildren(map);
+
+
+//button
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // myRef.setValue("test");
+            }
+        });
+
     }
 
-    //function returning "Date"
-    public void getDate() {
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat sDfr = new SimpleDateFormat("yyyy. MM. dd ");
-        String strDate = sDfr.format(calendar.getTime());
-        date.setText(strDate);
-        date.setTextColor(Color.BLUE);
-    }
-
-    /*Returning Wifi Name*/
-    public String returnWifiName() {
+    /*Function Returning Wifi Name*/
+    private String returnWifiName() {
         String ssid = "none";
         WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
@@ -95,12 +139,68 @@ public class MainActivity extends AppCompatActivity {
         return ssid;
     }
 
+    /*function returning "Date"*/
+    private void getDate() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sDfr = new SimpleDateFormat("yyyy. MM. dd ");
+        String strDate = sDfr.format(calendar.getTime());
+        date.setText(strDate);
+        date.setTextColor(Color.BLUE);
+    }
+
+    private void connectedToNetwork() {
+        wifiStatus.setText("You are connected");
+        wifiStatus.setTextColor(connectedColor);
+        checkedIn.setText("Yes");
+        checkedIn.setTextColor(connectedColor);
+    }
+
+    private void disconnectedToNetwork() {
+        wifiStatus.setText("You are  not connected");
+        wifiStatus.setTextColor(disconnectedColor);
+        checkedIn.setText("No");
+        checkedIn.setTextColor(disconnectedColor);
+        checkedOut.setText("yes");
+        checkedOut.setTextColor(connectedColor);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(nCReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(nCReceiver);
+
+    }
+
+    private BroadcastReceiver nCReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            /*Checking connectivity to a specific wifi*/
+            if (activeNetwork.isConnected()) {
+                String x = returnWifiName();
+                y = x.replaceAll("^\"|\"$", "");
+
+                if (y.equals(myNetworkName)) {
+                    connectedToNetwork();
+                } else if (activeNetwork.isConnected() && !(y.equals(myNetworkName))) {
+                    disconnectedToNetwork();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "Please turn ON your WI-FI", Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+
 }
 
-/*hiding time and status of checked_out
-         //group1 = findViewById(R.id.group);
-        //((View) group1).setVisibility(View.GONE);
 
-        //group2=findViewById(R.id.group2);
-        //group2.setVisibility(View.GONE);*/
+
 
